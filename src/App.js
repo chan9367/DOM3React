6c15dc05d7c9a34f56198ca29564552b75790472
Ke-vin Chan <chan9367@gmail.com>
2020-06-06 14:46:48 -0500

    added function color menu

--- src/App.js
+++ src/App.js
@@ -3,25 +3,19 @@ import './App.css';
 import addRow from './Components/addRow.js';
 import addColumn from './Components/addColumn.js';
 import clickColor from './Components/clickColor.js';
-import colorMenu from './Components/colorMenu.js';
+import ColorMenu from './Components/ColorMenu.js';
 
 
 class App extends Component {
   render(){
     return (
-        <div>
-          <select name="color" onchange="">
-            <option value="">-------</option>
-            <option value="red">Red</option>
-            <option value="orange">Orange</option>
-            <option value="yellow">Yellow</option>
-            <option value="green">Green</option>
-            <option value="blue">Blue</option>
-            <option value="purple">Purple</option>
-          </select>
-        </div>
       
-    );
+      <div>
+        <ColorMenu />
+        
+      </div>
+      
+    )
   }
 }
 export default App;
--- src/Components/colorMenu.js
+++ src/Components/colorMenu.js
@@ -1,29 +1,36 @@
 import React, { Component } from "react";
 
-class colorMenu extends Component {
+class ColorMenu extends Component {
     constructor(props){
         super(props);
         this.state={
-            show: true,
+            color: "",
         };
      }
  
+handleColor = (event) => {
+    this.setState({color: event.target.value})
+}
+
  render(){
      return (
-        <select name="color" onchange="">
-        <option value="">-------</option>
-        <option value="red">Red</option>
-        <option value="orange">Orange</option>
-        <option value="yellow">Yellow</option>
-        <option value="green">Green</option>
-        <option value="blue">Blue</option>
-        <option value="purple">Purple</option>
-        </select>
-     )
+     <div>
+         <label>Choose a color: </label>
+            <select name="color" onChange={this.handleColor}>
+                <option value="">-------</option>
+                <option value="red">Red</option>
+                <option value="orange">Orange</option>
+                <option value="yellow">Yellow</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+                <option value="purple">Purple</option>
+                {console.log("color: "+ this.state.color)}
+            </select>
+    </div>)
      
    }
 }
 
 
 
-export default colorMenu;
+export default ColorMenu;
