1272e5ce4370063790e1a0f47ba72112c25fb183
Ke-vin Chan <chan9367@gmail.com>
2020-06-06 13:40:07 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import addRow from './Components/addRow.js';
+import addColumn from './Components/addColumn.js';
+import clickColor from './Components/clickColor.js';
+import colorMenu from './Components/colorMenu.js';
+
+
+class App extends Component {
+  render(){
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+}
+export default App;
---
+++ src/Components/addColumn.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class addColumn extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            show: true,
+        };
+     }
+ 
+ render(){
+     return
+     
+   }
+}
+
+
+
+export default addColumn;
