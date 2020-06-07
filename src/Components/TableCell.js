bea8322e1ec854c0e0d59ed75d39ec4599ea4025
Konstantin Novichenko <novichenko.konstantin@gmail.com>
2020-06-06 23:34:32 -0400

    Added change color on click

--- src/Components/TableCell.js
+++ src/Components/TableCell.js
@@ -4,24 +4,29 @@ class TableCell extends Component {
     constructor(props){
         super(props);
         this.state={            
-            color: "",
+            color: "white",
         };
      }
  
- render(){
-     return(
-         <table>
-            <tbody>
-                <tr>
-                    <td className="table-cell"></td>
-                </tr>
-                
-            </tbody>
-         </table>
-         
-         
-     )     
-   }
+
+    changeColor = () =>{
+        this.setState({color: "red"});
+    }
+
+    render(){
+        return(
+            <table>
+                <tbody>
+                    <tr>
+                        <td style={{background: this.state.color}} className="table-cell" onClick={e => this.changeColor()}></td>
+                    </tr>
+                    
+                </tbody>
+            </table>
+            
+            
+        )     
+    }
 }
 
 
