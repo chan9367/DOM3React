4d98f3195e6722150a9fbef37f2462ff95fb1dc0
Ke-vin Chan <chan9367@gmail.com>
2020-06-06 19:05:11 -0500

    Updates

---
+++ precache-manifest.ba019b76c59a7f26b9cd78b06c991c0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d950280b564234b25412431b8bacb455",
+    "url": "/DOM3React/index.html"
+  },
+  {
+    "revision": "ed8430954d37f8ee7462",
+    "url": "/DOM3React/static/css/main.ed5aae23.chunk.css"
+  },
+  {
+    "revision": "33ed740e3558bed0abdc",
+    "url": "/DOM3React/static/js/2.2d834d77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DOM3React/static/js/2.2d834d77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed8430954d37f8ee7462",
+    "url": "/DOM3React/static/js/main.742a9f6d.chunk.js"
+  },
+  {
+    "revision": "0c0ae82492924a35da6b",
+    "url": "/DOM3React/static/js/runtime-main.8ebafc0c.js"
+  }
+]);
